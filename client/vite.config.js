8e6e853a2cc9932d6214dd40ea420b93c6b6b7fa
ashmeet2003 <161520664+ashmeet2003@users.noreply.github.com>
2024-08-17 04:45:56 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,15 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://sociox-backend.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
